c9afa9af41a51c8497348483f73f430765bcae85
tanqilin@gmail.com <tanqilin@gmail.com@7f9f918b-d3ad-68a3-a3fd-0ccaecf1a7ad>
2011-12-26 14:44:28 +0000

    Update the script to support Python3.x 1. use subprocess instead of commands 2. remove the print statement.
    git-svn-id: https://xianglin.googlecode.com/svn/trunk@8 7f9f918b-d3ad-68a3-a3fd-0ccaecf1a7ad

--- parse_stack/src/parse_stack.py
+++ parse_stack/src/parse_stack.py
@@ -18,7 +18,8 @@
 import sys
 import re
 import os
-import commands
+import subprocess
+from subprocess import Popen 
 from optparse import OptionParser
 
 sohead = re.compile('(.+\.so):')
@@ -27,11 +28,14 @@ funcline = re.compile('^[ ]+([0-9a-f]+):.+')
 crashline = re.compile('.+pc.([0-9a-f]{8})\s(.+?\.so).*')
 
 def shellcmd(command):
-    status, rs = commands.getstatusoutput(command);
-    if status == 0:
-        return rs
-    else:
-        return -1;
+    try:
+        p = Popen(command, stdout=subprocess.PIPE, shell=True)
+    except ValueError:
+        return -1
+    status = p.wait()
+    if status != 0:
+        return -1
+    return p.stdout
 
 def parsestack(lines):
     ret = []
@@ -55,7 +59,7 @@ def parsestackline(line):
 def find_addr2line():
     ndk_home_path = os.environ["ANDROID_NDK_HOME"]
     if ndk_home_path == None:
-        print >> sys.stderr, "Don't find the ANDROID_NDK_HOME environment in you OS"
+        sys.stderr.write("Don't find the ANDROID_NDK_HOME environment in you OS\n");
         return None
 
     return search_file(os.path.join(ndk_home_path, "toolchains"), "^arm.*addr2line$", True)
@@ -80,21 +84,19 @@ def search_file(directory, name, regular = False):
 
 def execute_addr2line(addr2line, libpath, addr):
     command = "%s -f -e %s %x" %(addr2line, libpath, addr)
-    status, rs = commands.getstatusoutput(command)
-    if status != 0:
+    fp = shellcmd(command)
+    if fp == -1:
         return (None, None, None)
-    pos1 = rs.find("\n")
-    pos2 = rs.find(":")
-    func = rs[0:pos1].strip()
-    src = rs[pos1:pos2].strip()
-    line = rs[pos2+1:].strip()
-    return (func, src, line)
+    func = fp.readline().strip()
+    src = fp.readline().strip()
+    src = src.split(":")
+    return (func, src[0], src[1])
 
 def print_crash_stack(output, rs):
-    print >> output, "Crash statck ==================================================="
+    output.write("Crash statck ===================================================\n")
     for line in rs:
-      print >> output, "%s" %(line)
-    print >> output, "Crash statck ==================================================="
+        output.write("%s\n" %(line))
+    output.write("Crash statck ===================================================\n")
 
 def parse_crash_stack(options, addr2line, stack, output):
 
@@ -123,45 +125,48 @@ def parse_crash_stack(options, addr2line, stack, output):
     if len(parse_rs) > 0:
         print_crash_stack(output, parse_rs)
         parse_rs = []
+    return 0
 
 def getPID(process_name):
     command = "adb shell ps | grep %s" % (process_name)
-    rs = shellcmd(command)
-    if rs == -1:
-        print >> sys.stderr, "Don't get the PID of process %s, please make sure the given name is right" % (process_name)
+    fp = shellcmd(command)
+    if fp == -1:
+        errMsg = "Don't get the PID of process %s, please make sure the given name is right\n" % (process_name)
+        sys.stderr.write(errMsg)
         return -1
     
-    rs = rs.split("\n")
+    rs = fp.readlines()
     if len(rs) != 1:
         pids = []
         for i in rs:
             p = i.split()
             if p[len(p) - 1] == process_name:
-               return int(p[1])
+                return int(p[1])
             pids.append(i.split()[1])
-        print >> sys.stderr, "Get more then one PID (%s), please given a more accurate process name" % (", ".join(pids))
+        errMsg = "Get more then one PID (%s), please given a more accurate process name\n" % (", ".join(pids))
+        sys.stderr.write(errMsg) 
         return -1
     return int(rs[0].split()[1])
 
 def getSoMaps(pid):
     command = "adb shell cat /proc/%d/maps" % (pid)
-    rs = shellcmd(command)
-    if rs == -1:
-        print "error in get so maps"
-        return -1
-    rs = rs.split("\n")
+    fp = shellcmd(command)
+    if fp == -1:
+        print("error in get so maps")
+        return None
+    rs = fp.readlines()
     so_maps = {}
     for i in rs:
         if i.find("/data/data/") == -1:
-          continue;
+            continue;
 
         pos = i.find("-")
         if (pos == -1):
-          continue
+            continue
         addr = int(i[0:pos], 16)
         pos = i.rfind("/")
         if (pos == -1):
-          continue
+            continue
         so_name = i[pos+1:].strip()
         if not so_name in so_maps.keys():
             so_maps[so_name] = addr
@@ -169,11 +174,15 @@ def getSoMaps(pid):
 
 def parse_memory_stack(options, addr2line, alloc, output):
     if options.process == None:
-        print >> sys.stderr, "The process name is required for parse memory stack"
+        sys.stderr.write("The process name is required for parse memory stack\n")
         return -1
     pid = getPID(options.process)
+    if pid == -1:
+        return -1
     so_maps = getSoMaps(pid)
-
+    if not so_maps:
+        sys.stderr.write("Don't find the libraries of pid %d\n" % (pid))
+        return -1
     for line in alloc:
         if line.find("/data/data") == -1:
             output.write(line)
@@ -197,7 +206,7 @@ def parse_memory_stack(options, addr2line, alloc, output):
                 output.write("%s\t%s %s %s\n" %(line.rstrip(), func, fs, line_num))
             else:
                 output.write(line)
-
+    return 0
 if __name__=="__main__":
 
     parser = OptionParser(usage="%prog -s <path> [-d <file>] [-a <file> -p <process> [-o <file>]] [--android_ndk_home=<path>]" )
@@ -211,12 +220,12 @@ if __name__=="__main__":
     (options, args) = parser.parse_args()
     
     if options.symbols is None:
-        print >> sys.stderr, "The symbols is required"
+        sys.stderr.write("The symbols is required\n")
         parser.print_help();
         exit(1)
     else:
         if not os.path.exists(options.symbols):
-            print >> sys.stderr, "The symbols is not existed" %(options.symbols)
+            sys.stderr.write("The symbols is not existed\n" %(options.symbols))
             exit(1)
 
     if options.dump is not None:
@@ -237,15 +246,16 @@ if __name__=="__main__":
         if os.path.exists(options.ndk_home):
             addr2line = search_file(os.path.join(options.ndk_home, "toolchains"), "^arm.*addr2line$", True)
         else:
-            print >> sys.stderr, "The android_ndk_home is incorrect path"
+            sys.stderr.write("The android_ndk_home is incorrect path\n")
             exit(1)
     else:
         addr2line = find_addr2line()
     if addr2line is None:
-        print >> sys.stderr, "Can find the addr2line command from the ANDROID_NDK_HOME."
+        sys.stderr.write("Can find the addr2line command from the ANDROID_NDK_HOME.\n")
         exit(1)
 
     if alloc is not None:
-        parse_memory_stack(options, addr2line, alloc, output)
+        rs = parse_memory_stack(options, addr2line, alloc, output)
     else:
-        parse_crash_stack(options, addr2line, stack, output)
+        rs = parse_crash_stack(options, addr2line, stack, output)
+    exit(rs)
